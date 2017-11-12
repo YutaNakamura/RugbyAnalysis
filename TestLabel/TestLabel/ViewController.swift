41e0ee01aa901e4bea597e2fee63ea1a3ce52887
中村優太 <yuta@nakamurayuutanoMacBook-Pro.local>
2017-11-12 22:56:47 +0900

    SwiftAppの作成

---
+++ TestLabel/TestLabel/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestLabel
+//
+//  Created by 中村優太 on 2017/11/12.
+//  Copyright © 2017年 中村優太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
